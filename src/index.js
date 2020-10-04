f1e8071436ab6976494af6554c11587ee30f6aab
ilara90 <68242308+ilara90@users.noreply.github.com>
2020-10-04 21:12:13 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    var arr = [];
+    for(var temp = n;Math.round(temp)!=0;temp/=10,temp = Math.floor(temp)){
+  arr.unshift(temp%10);
+  };
+    let arrNew = arr.reverse();
+    let result = arrNew.join("");
+    return Number(result);
 }
