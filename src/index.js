3b337c33ca9ebe303da23f58a7745bf4a57ccf6e
ilara90 <68242308+ilara90@users.noreply.github.com>
2020-10-05 18:48:03 +0300

    fix: fixed mistake

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 module.exports = function reverse (n) {
+    var num = Math.abs(n);
     var arr = [];
-    for(var temp = n;Math.round(temp)!=0;temp/=10,temp = Math.floor(temp)){
-  arr.unshift(temp%10);
-  };
+    for(var temp = num;Math.round(temp)!=0;temp/=10,temp = Math.floor(temp)){
+      arr.unshift(temp%10);
+    };
     let arrNew = arr.reverse();
+    if(arrNew[0] === 0){
+      arrNew.splice(0, 1)
+    } else {
+      arrNew
+    }
     let result = arrNew.join("");
     return Number(result);
 }
